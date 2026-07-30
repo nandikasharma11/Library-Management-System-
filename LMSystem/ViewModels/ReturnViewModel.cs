f5e1814afaa810bc98b5a7636170078ab5615794
Nandika Sharma <nandikasharma11@icloud.com>
2026-07-30 20:30:37 +0530

    Commit 3: Book Borrowing & Returning Module

---
+++ LMSystem/ViewModels/ReturnViewModel.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSystem.ViewModels
+{
+    public class ReturnViewModel
+    {
+        [Required]
+        public int BorrowRecordId { get; set; }
+
+        [BindNever]
+        public string? BookTitle { get; set; }
+
+        [BindNever]
+        public string? BorrowerName { get; set; }
+
+        [BindNever]
+        public DateTime? BorrowDate { get; set; }
+    }
+}
